ca1125195e3ebef8251f239b88738c7841ecd4e0
sss-kato <syunpei.1124@gmail.com>
2021-07-25 17:18:28 +0900

    create model and expand admin page

--- app_blog/blogs/views.py
+++ app_blog/blogs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 def index(request):
-    return render(request,'blogs/index.html')
+    blogs = Blog.objects.order_by('-created_datetime')
+    print(blogs)
+    return render(request, 'blogs/index.html', {'blogs': blogs})
+
